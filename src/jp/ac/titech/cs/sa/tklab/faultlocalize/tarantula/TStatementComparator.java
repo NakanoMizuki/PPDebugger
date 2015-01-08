ac6cf7821bdc28fc6a455d56ebfc886ae14febd7
NakanoMizuki <mizuki590@gmail.com>
2015-01-08 16:24:04 +0900

    modified tarantula

--- src/jp/ac/titech/cs/sa/tklab/faultlocalize/tarantula/TStatementComparator.java
+++ src/jp/ac/titech/cs/sa/tklab/faultlocalize/tarantula/TStatementComparator.java
@@ -14,10 +14,14 @@ class TStatementComparator implements Comparator<TStatement> {
 	public int compare(TStatement o1, TStatement o2) {
 		if(o1.getSuspicious() < o2.getSuspicious()){
 			return 1;
-		}else if((o1.getSuspicious() == o2.getSuspicious())
-					&& (o1.getLineNumber() > o2.getLineNumber())){
-			return 1;
+		}else if((o1.getSuspicious() == o2.getSuspicious())){
+			if(o1.getLineNumber() > o2.getLineNumber()){
+				return 1;
+			}else{
+				return -1;
+			}
+		}else{
+			return -1;
 		}
-		return -1;
 	}
 }
