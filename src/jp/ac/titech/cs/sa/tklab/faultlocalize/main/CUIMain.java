e1a61c756b0e8d1ddb65d8bf55aba9ff5335a3fc
NakanoMizuki <mizuki590@gmail.com>
2015-01-08 15:29:05 +0900

    modified main

--- src/jp/ac/titech/cs/sa/tklab/faultlocalize/main/CUIMain.java
+++ src/jp/ac/titech/cs/sa/tklab/faultlocalize/main/CUIMain.java
@@ -1,14 +1,10 @@
 package jp.ac.titech.cs.sa.tklab.faultlocalize.main;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXBException;
--- src/jp/ac/titech/cs/sa/tklab/faultlocalize/main/GUIMain.java
+++ src/jp/ac/titech/cs/sa/tklab/faultlocalize/main/GUIMain.java
@@ -6,10 +6,8 @@ import javax.swing.JFileChooser;
 import javax.xml.bind.JAXBException;
 
 import jp.ac.titech.cs.sa.tklab.faultlocalize.out.OutToFile;
-import jp.ac.titech.cs.sa.tklab.faultlocalize.out.SystemOut;
 import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.PPDebugger;
 import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.model.result.Result;
-import jp.ac.titech.cs.sa.tklab.faultlocalize.tarantula.Tarantula;
 
 /**
  * 
@@ -17,38 +15,23 @@ import jp.ac.titech.cs.sa.tklab.faultlocalize.tarantula.Tarantula;
  *
  */
 public class GUIMain {
-
-	//ファイル選択時のデフォルトディレクトリ
-	private static final String DIRECTORY = "C:/Users/nakano/Desktop";
-	//private static final String DIRECTORY = "C:/Users/nakano/Dropbox/research/ohnuma/data/xml";
+	
+	private static final String DIRECTORY = "C:/Users/nakano/Desktop";	//ファイル選択時のデフォルトディレクトリ
+	private static final int HOPNUM = 5;	//伝播数
 	
 
-	private static final int HOPNUM = 5;
-	
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		GUIMain main = new GUIMain();
 		main.start();
 	}
 
 	private void start(){
-		OutToFile out  = new OutToFile("C:/Users/nakano/Desktop/ppdebugger.txt");
-		
+		OutToFile out  = new OutToFile(DIRECTORY + "/ppdebugger.txt");
 		File[] passedFiles = getFiles();
 		File[] failedFiles = getFiles();
-		//Tarantula tarantula = new Tarantula(new SystemOut());
-		//Tarantula tarantula = new Tarantula(new OutToFile("C:/Users/nakano/Desktop/experiment/file.txt"));
 		PPDebugger ppdebugger = new PPDebugger(HOPNUM);
 		
-		
-		
 		try {
-			//tarantula.learn(passedFiles, failedFiles);
-			//tarantula.printRanking(RANKNUM);
-			//tarantula.printAllRanking();
 			ppdebugger.learn(passedFiles);
 			ppdebugger.printAllRanking(out);
 			for(Result result: ppdebugger.createResults(failedFiles)){
