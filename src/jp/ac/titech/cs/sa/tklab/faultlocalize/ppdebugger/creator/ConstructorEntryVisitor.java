a37ea10738bca37874891a0878e57283b27465ac
NakanoMizuki <mizuki590@gmail.com>
2015-01-19 14:28:46 +0900

    add:constructor param

--- src/jp/ac/titech/cs/sa/tklab/faultlocalize/ppdebugger/creator/ConstructorEntryVisitor.java
+++ src/jp/ac/titech/cs/sa/tklab/faultlocalize/ppdebugger/creator/ConstructorEntryVisitor.java
@@ -6,6 +6,12 @@ import jp.ac.nagoya_u.is.i.agusa.person.knhr.bxmodel.ConstructorEntry;
 import jp.ac.nagoya_u.is.i.agusa.person.knhr.bxmodel.MethodSignature;
 import jp.ac.nagoya_u.is.i.agusa.person.knhr.bxmodel.ObjectInfoType;
 import jp.ac.nagoya_u.is.i.agusa.person.knhr.bxmodel.PrimitiveValueInfo;
+import jp.ac.nagoya_u.is.i.agusa.person.knhr.bxmodel.VariableDefinition;
+import jp.ac.titech.cs.sa.tklab.faultlocalize.StatementData;
+import jp.ac.titech.cs.sa.tklab.faultlocalize.StatementDataFactory;
+import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.DataDependencyFactory;
+import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.model.DataDependency;
+import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.model.DataDependencySet;
 import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.model.execution.ExecutionModel;
 import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.model.execution.LineVariable;
 import jp.ac.titech.cs.sa.tklab.faultlocalize.ppdebugger.model.execution.Variable;
@@ -26,14 +32,22 @@ class ConstructorEntryVisitor {
 		return false;
 	}
 	
-	private static void createDataDependency(ExecutionModel model,ConstructorEntry ce,LineVariable argsLine,Scope scope){
-		MethodSignature signature = ce.getMethodSignature();
+	private static void createDataDependency(ExecutionModel model,ConstructorEntry entry,LineVariable argsLine,Scope scope){
+		MethodSignature signature = entry.getMethodSignature();
 		List<String> argumentTypes = signature.getArgumentTypes().getTypeNames();
-		List<Object> argumentValues = ce.getArgumentValues().getPrimitiveValueInfosAndObjectInfos();
+		List<Object> argumentValues = entry.getArgumentValues().getPrimitiveValueInfosAndObjectInfos();
 		List<Variable> variables = argsLine.getVariables();
-		
+		StatementDataFactory sdFactory = StatementDataFactory.getInstance();
+		DataDependencyFactory ddFactory = DataDependencyFactory.getInstance();
+
 		int stackAddress = 0;		//スタックの何番目に格納される引数か。
 		for(int i=0; i < argumentTypes.size(); i++){
+			//ローカル変数と仮引数を対応付ける
+			String paramName = NameCreator.createActualParamName(scope, i);
+			String localName = NameCreator.createFormalParamName(scope, stackAddress);
+			scope.putParam(localName, paramName);
+			
+			//引数間でのデータ依存を発生させる
 			String type = argumentTypes.get(i);
 			int index = -1;
 			if(EntryUtil.isPrimitiveType(type)){
@@ -44,10 +58,22 @@ class ConstructorEntryVisitor {
 				index = EntryUtil.match(value, variables);
 			}
 			if(index != -1){
-				Variable var = new Variable(NameCreator.createFormalParamName(scope, stackAddress), variables.get(index).getLatestDefinition());
-				model.addVariable(var);
+				Variable refferedVariable = variables.get(index);
 				variables.remove(index);
+				
+				//仮引数として新たな変数を追加
+				VariableDefinition definition = EntryUtil.createParamDefinition(entry,scope.getMethodName(), i, paramName);
+				Variable actualVariable = new Variable(paramName, definition);
+				model.addVariable(actualVariable);
+				
+				//実引数から仮引数へのデータ依存
+				StatementData formalParamSD = EntryUtil.createFormalParamSD(entry,scope.getMethodName(), i, paramName);
+				StatementData sdData = sdFactory.genStatementData(refferedVariable.getLatestDefinition().getSourcePath(), refferedVariable.getLatestDefinition().getLineNumber(),refferedVariable.getLatestDefinition().getThread());
+				DataDependency dd = ddFactory.genDataDependency(refferedVariable.getVarName(),sdData );
+				DataDependencySet dds = new DataDependencySet(formalParamSD,dd, entry.getEventNumber());
+				model.addDataDependencySet(formalParamSD, dds);
 			}
+			
 			stackAddress += EntryUtil.getBlockSize(type);
 		}
 	}
