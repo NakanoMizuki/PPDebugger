dc51db6f44873935d26f9db1eda42faf1c114f9c
NakanoMizuki <mizuki590@gmail.com>
2015-01-30 19:25:35 +0900

    change:passed&failed parcent

--- src/jp/ac/titech/cs/sa/tklab/faultlocalize/ppdebugger/PPDebugger.java
+++ src/jp/ac/titech/cs/sa/tklab/faultlocalize/ppdebugger/PPDebugger.java
@@ -85,10 +85,7 @@ public class PPDebugger{
 				}else{
 					prob = 0;
 					for(DataDependencySet dds: failedSt.getDataDependencySets()){
-						double current = failedSt.getProb(dds)- passedSt.getProb(dds);
-						if(current < 0){
-							current =0;
-						}
+						double current = failedSt.getProb(dds)/(failedSt.getProb(dds) + passedSt.getProb(dds));
 						prob = Math.max(prob, current);
 					}
 				}
